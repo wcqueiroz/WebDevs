f480661ddf7369b2625c8701844261c3a99ce3ed
Wellington <wellingtonqueiroz@gmail.com>
2018-07-17 22:21:43 -0300

    Correção da quebra de linha observando o guia de estilo PEP8

--- p1/entertainment_center.py
+++ p1/entertainment_center.py
@@ -12,46 +12,43 @@ def filmes():
     Lista de filmes e seus atributos.
     """
     espera_de_um_milagre = media.Movie(u"À Espera de um Milagre",
-                                       u"A história de um preso no corredo"\
-                                       "r da morte com um dom especial de "\
-                                       "cura",
-                                       "https://upload.wikimedia.org/wikip"\
-                                       "edia/pt/8/8f/%C3%80_Espera_de_um_M"\
-                                       "ilagre.jpg",
-                                       "https://www.youtube.com/watch?v=EI"\
-                                       "c_wJtm6AU")
+                                       u"A história de um preso no corredor da"
+                                       " morte com um dom especial de cura",
+                                       "https://upload.wikimedia.org/wikipedia"
+                                       "/pt/8/8f/%C3%80_Espera_de_um_Milagre.j"
+                                       "pg",
+                                       "https://www.youtube.com/watch?v=EIc_wJ"
+                                       "tm6AU")
     intocaveis = media.Movie(u"Intocáveis",
-                             u"Um rico tetraplégico contrata um assistente"\
-                             " sem experiência em cuidar de pessoas e junt"\
-                             "os vivem muitas aventuras",
-                             "https://upload.wikimedia.org/wikipedia/pt/d/"\
-                             "d6/Intouchables_cartaz.jpg",
+                             u"Um rico tetraplégico contrata um assistente sem"
+                             " experiência em cuidar de pessoas e juntos vivem"
+                             " muitas aventuras",
+                             "https://upload.wikimedia.org/wikipedia/pt/d/d6/I"
+                             "ntouchables_cartaz.jpg",
                              "https://www.youtube.com/watch?v=-Fb8h4gChlU")
     django_livre = media.Movie(u"Django Livre",
-                               u"Django é um escravo que é libertado por u"\
-                               "m caçador de recompensas e com sede de vin"\
-                               "gança decide ajudar o seu libertador na ca"\
-                               "çada de recompensas",
-                               "https://upload.wikimedia.org/wikipedia/pt/"\
-                               "8/8b/Django_Unchained_Poster.jpg",
-                               "https://www.youtube.com/watch?v=tivv135aGb"\
-                               "c")
+                               u"Django é um escravo que é libertado por um ca"
+                               "çador de recompensas e com sede de vingança de"
+                               "cide ajudar o seu libertador na caçada de reco"
+                               "mpensas",
+                               "https://upload.wikimedia.org/wikipedia/pt/8/8b"
+                               "/Django_Unchained_Poster.jpg",
+                               "https://www.youtube.com/watch?v=tivv135aGbc")
     matrix = media.Movie(u"Matrix",
-                         u"Matrix é um sistema inteligente e artificial qu"\
-                         "e escraviza a humanidade manipulando suas mentes",
-                         "https://upload.wikimedia.org/wikipedia/pt/c/c1/T"\
-                         "he_Matrix_Poster.jpg",
+                         u"Matrix é um sistema inteligente e artificial que es"
+                         "craviza a humanidade manipulando suas mentes",
+                         "https://upload.wikimedia.org/wikipedia/pt/c/c1/The_M"
+                         "atrix_Poster.jpg",
                          "https://www.youtube.com/watch?v=m8e-FF8MsqU")
     auto_da_compadecida = media.Movie(u"O Auto da Compadecida",
-                                      u"Dois sertanejos pobres, um é menti"\
-                                      "roso e o outro é covarde, vivem as "\
-                                      "maiores aventuras para conquistar o"\
-                                      " pão de cada dia",
-                                      "https://upload.wikimedia.org/wikipe"\
-                                      "dia/pt/b/bf/O_auto_da_compadecida.j"\
-                                      "pg",
-                                      "https://www.youtube.com/watch?v=XPu"\
-                                      "Mu_ENzlg")
+                                      u"Dois sertanejos pobres, um é mentiroso"
+                                      " e o outro é covarde, vivem as maiores "
+                                      "aventuras para conquistar o pão de cada"
+                                      " dia",
+                                      "https://upload.wikimedia.org/wikipedia/"
+                                      "pt/b/bf/O_auto_da_compadecida.jpg",
+                                      "https://www.youtube.com/watch?v=XPuMu_E"
+                                      "Nzlg")
     movies = [
         espera_de_um_milagre,
         intocaveis,
